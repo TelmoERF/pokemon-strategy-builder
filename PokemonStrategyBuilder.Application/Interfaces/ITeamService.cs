766b61d0499217e89795d6bd784b85687f93fbd9
TelmoERF <telmoferreira96@gmail.com>
2026-03-16 21:15:05 +0000

    Add team rating feature based on weakness analysis

--- PokemonStrategyBuilder.Application/Interfaces/ITeamService.cs
+++ PokemonStrategyBuilder.Application/Interfaces/ITeamService.cs
@@ -10,4 +10,5 @@ public interface ITeamService
     Task<IReadOnlyCollection<TypeWeaknessDto>?> AnalyzeTeamAsync(int teamId, CancellationToken cancellationToken = default);
     Task<TeamDto?> UpdateAsync(int id, UpdateTeamRequestDto request, CancellationToken cancellationToken = default);
     Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
+    Task<TeamRatingDto?> GetRatingAsync(int id, CancellationToken cancellationToken = default);
 }
